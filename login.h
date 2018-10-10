37bcebf199e065dcaa7ab9bd47a76c13f2971b8b
EarvinJones <shjearvin@outlook.com>
2018-10-10 21:46:09 +0800

    version1.1

--- login.h
+++ login.h
@@ -1,6 +1,7 @@
 #ifndef LOGIN_H
 #define LOGIN_H
-
+#include"QtWidgets"
+#include"QtSql"
 #include <QMainWindow>
 
 namespace Ui {
--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include "login.h"
 #include <QApplication>
+#include"QtWidgets"
+#include"QtSql"
 
 int main(int argc, char *argv[])
 {
+    QSqlDatabase db=QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("127.0.0.1");
+    db.setPort(3306);
+    db.setDatabaseName("tdb");
+    db.setUserName("EarvinJones");
+    db.setPassword("1064");
+//    if(db.open())qDebug()<<"success";
+//    else qDebug()<<db.lastError();
+
     QApplication a(argc, argv);
     login w;
     w.show();
